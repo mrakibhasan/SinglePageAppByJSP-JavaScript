8f8fd1d322c794af35c2bdb12d966387b4c3c6ad
mrakibhasan <rakibcse05@gmail.com>
2017-02-25 18:36:51 -0500

    change data config

--- src/com/demo/dao/UserDao.java
+++ src/com/demo/dao/UserDao.java
@@ -18,8 +18,8 @@ public class UserDao {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/dec2016", "root",
-					"Raha_634709");
+					"jdbc:mysql://mysqldatabaseaws.cqd7nfsrkttf.us-west-2.rds.amazonaws.com/testdb", "masterUserName",
+					"masterPassword");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
